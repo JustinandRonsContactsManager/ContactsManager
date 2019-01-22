eebf3e74bd429190bdf45884ff61627d9ff681e3
Ron Soeum <ronsoeum@gmail.com>
2019-01-22 15:07:14 -0600

    added menu method

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,2 +1,22 @@
+
+
+
 public class ContactsManager {
-}
+
+    public static void main(String[] args) {
+
+    }// main
+
+
+    public static void menu(){
+        System.out.println("Welcome to Contacts Manager\n");
+        System.out.println("1: View all contacts");
+        System.out.println("2: Add a new contact");
+        System.out.println("3: Search a contact by his/her name");
+        System.out.println("4: Delete an existing contact");
+    }
+
+
+
+
+}// class
