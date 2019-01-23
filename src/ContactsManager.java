18c23a5737cd6547057cf049a81c716c3cea20ef
allenjustinblanco <blanco.allenjustin@gmail.com>
2019-01-23 09:19:24 -0600

    Added comments and setting up for refactor

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -2,8 +2,9 @@ import java.util.List;
 import java.io.IOException;
 
 public class ContactsManager {
-    String directory = "data";
+    String directory = "src/data";
     String file = "contacts.txt";
+    Path contactsPath = Paths.get(directory);
 
     public static void main(String[] args) {
         start();
@@ -22,33 +23,73 @@ public class ContactsManager {
 
         if(userInput == 1){
           //view all contacts
+          viewContacts();
         } else if( userInput == 2) {
           //add a new contact
+          addContact();
         } else if( userInput == 3) {
           //search a contact by name
+          searchContact();
         } else if( userInput == 4) {
           //delete an extisting contact
+          deleteContact();
         }
     }//menu
+
+    private String contact;
+    private Int number;
+
+    public Contact(String Contact) {
+      this.contact = contact;
+      this.number = number;
+    }//Contact
+
+    public String getContact() {
+      return contact;
+    }//getContact
+
+    public Int getNumber() {
+      return number;
+    }//getNumber
+    //setters
+    public void setNumber(Int number) {
+      this.number = number;
+    }//setNumber
+
+    public void setContact(String contact) {
+      this.contact = contact;
+    }//setContact
 
     public static void start(){
         menu();
     }//start
 
     public static void viewContacts(){
-      Path contactsPath = Paths.get(directory, file);
       //write file path
+
+      List<String> Files.readAllLines(Path contactsPath);
       //catch and create file if not found
-    }
+      if(Files.notExists(Path contactsPath)){
+        Path Files.creatFile(Path contactsPath);
+      }
+
+    }//viewContacts
     public static void addContact(){
-      //
-    }
+      //initialize contactsList
+      List<String> contactsList;
+      Path contactsPath = Paths.get("data", "contacts.txt");
+      String newContactName = input.getString("Enter a new Contact Name");
+      Int newPhoneNumber = input.getInt("Enter a new Phone number for " + newContactName);
+      Files.write(contactsPath, Arrays.listOf(contactsList));
+
+    }//addContact
     public static void searchContact(){
 
-    }
+    }//searchContact
     public static void deleteContact(){
 
-    }
+    }//deleteContact
+
 
 
 }// class
